4d122caa1a614e5f0d3ef65eec694cfc9f038335
Eslam <eslam.muhammed.saeed@gmail.com>
2021-05-12 16:56:02 +0200

    Procfile for Heroku5

--- resources/views/vendor/voyager/dashboard/sidebar.blade.php
+++ resources/views/vendor/voyager/dashboard/sidebar.blade.php
@@ -4,19 +4,16 @@
             <div class="navbar-header">
                 <a class="navbar-brand" href="{{ route('voyager.dashboard') }}">
                     <div class="logo-icon-container">
-                        <?php $admin_logo_img = Voyager::setting('admin.icon_image', ''); ?>
-                        @if($admin_logo_img == '')
-                            <img width="40px" height="40px" src="{{ voyager_asset('images/logo-icon-light.png') }}" alt="Logo Icon">
-                        @else
-                            <img src="{{ Voyager::image($admin_logo_img) }}" alt="Logo Icon">
-                        @endif
+                        
+                            <img width="40px" height="40px" src="{{ asset('images/mekeg_logo.png') }}" alt="Logo Icon">
+                        
                     </div>
-                    <div class="title">{{Voyager::setting('admin.title', 'VOYAGER')}}</div>
+                    <div class="title">MEKEG</div>
                 </a>
             </div><!-- .navbar-header -->
 
             <div class="panel widget center bgimage"
-                 style="background-image:url({{ Voyager::image( Voyager::setting('admin.bg_image'), voyager_asset('images/bg.jpg') ) }}); background-size: cover; background-position: 0px;">
+                 style="background-image:url({{ asset('images/background_image.png') }}); background-size: cover; background-position: 0px;">
                 <div class="dimmer"></div>
                 <div class="panel-content">
                     <img src="{{ $user_avatar }}" class="avatar" alt="{{ Auth::user()->name }} avatar">
