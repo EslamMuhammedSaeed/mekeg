b5c207d0880a2ee9e0b6d9c44f18b8bdf89f269c
Eslam <eslam.muhammed.saeed@gmail.com>
2021-05-15 04:12:15 +0200

    Procfile for Heroku6

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,17 +1,111 @@
-@extends('auth.master')
+@extends('voyager::auth.master')
 
 @section('content')
-    
+    <div class="login-container">
+
+        <p>{{ __('voyager::login.signin_below') }}</p>
+
+        <form action="{{ route('voyager.login') }}" method="POST">
+            {{ csrf_field() }}
+            <div class="form-group form-group-default" id="emailGroup">
+                <label>{{ __('voyager::generic.email') }}</label>
+                <div class="controls">
+                    <input type="text" name="email" id="email" value="{{ old('email') }}" placeholder="{{ __('voyager::generic.email') }}" class="form-control" required>
+                </div>
+            </div>
+
+            <div class="form-group form-group-default" id="passwordGroup">
+                <label>{{ __('voyager::generic.password') }}</label>
+                <div class="controls">
+                    <input type="password" name="password" placeholder="{{ __('voyager::generic.password') }}" class="form-control" required>
+                </div>
+            </div>
+
+            <div class="form-group" id="rememberMeGroup">
+                <div class="controls">
+                    <input type="checkbox" name="remember" id="remember" value="1"><label for="remember" class="remember-me-text">{{ __('voyager::generic.remember_me') }}</label>
+                </div>
+            </div>
+
+            <button type="submit" class="btn btn-block login-button">
+                <span class="signingin hidden"><span class="voyager-refresh"></span> {{ __('voyager::login.loggingin') }}...</span>
+                <span class="signin">{{ __('voyager::generic.login') }}</span>
+            </button>
+
+            @if (Route::has('password.request'))
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                        {{ __('Forgot your password?') }}
+                    </a>
+            @endif
+
+        </form>
+
+        <div style="clear:both"></div>
+
+        @if(!$errors->isEmpty())
+            <div class="alert alert-red">
+                <ul class="list-unstyled">
+                    @foreach($errors->all() as $err)
+                        <li>{{ $err }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+    </div> <!-- .login-container -->
+@endsection
+
+@section('post_js')
+
+    <script>
+        var btn = document.querySelector('button[type="submit"]');
+        var form = document.forms[0];
+        var email = document.querySelector('[name="email"]');
+        var password = document.querySelector('[name="password"]');
+        btn.addEventListener('click', function(ev){
+            if (form.checkValidity()) {
+                btn.querySelector('.signingin').className = 'signingin';
+                btn.querySelector('.signin').className = 'signin hidden';
+            } else {
+                ev.preventDefault();
+            }
+        });
+        email.focus();
+        document.getElementById('emailGroup').classList.add("focused");
+
+        // Focus events for email and password fields
+        email.addEventListener('focusin', function(e){
+            document.getElementById('emailGroup').classList.add("focused");
+        });
+        email.addEventListener('focusout', function(e){
+            document.getElementById('emailGroup').classList.remove("focused");
+        });
+
+        password.addEventListener('focusin', function(e){
+            document.getElementById('passwordGroup').classList.add("focused");
+        });
+        password.addEventListener('focusout', function(e){
+            document.getElementById('passwordGroup').classList.remove("focused");
+        });
+
+    </script>
+@endsection
 
 
-        <!-- Session Status -->
+{{-- @extends('auth.master')
+
+@section('content')
+     --}}
+
+
+        {{-- <!-- Session Status -->
         <x-auth-session-status class="mb-4" :status="session('status')" />
 
         <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
         <form method="POST" action="{{ route('login') }}">
-            @csrf
+            @csrf --}}
 
             <!-- Email Address -->
             {{-- <div class="form-group form-group-default" >
@@ -20,12 +114,12 @@
                 <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
             </div> --}}
 
-            <div class="form-group form-group-default" id="emailGroup">
+            {{-- <div class="form-group form-group-default" id="emailGroup">
                 <label>{{ __('voyager::generic.email') }}</label>
                 <div class="controls">
                     <input type="text" name="email" id="email" value="{{ old('email') }}" placeholder="{{ __('voyager::generic.email') }}" class="form-control" required>
                 </div>
-            </div>
+            </div> --}}
 
             <!-- Password -->
             {{-- <div class="form-group form-group-default">
@@ -36,12 +130,12 @@
                                 name="password"
                                 required autocomplete="current-password" />
             </div> --}}
-            <div class="form-group form-group-default" id="passwordGroup">
+            {{-- <div class="form-group form-group-default" id="passwordGroup">
                 <label>{{ __('voyager::generic.password') }}</label>
                 <div class="controls">
                     <input type="password" name="password" placeholder="{{ __('voyager::generic.password') }}" class="form-control" required>
                 </div>
-            </div>
+            </div> --}}
 
             <!-- Remember Me -->
             {{-- <div class="block mt-4">
@@ -51,7 +145,7 @@
                 </label>
             </div> --}}
 
-            <div class="form-group" id="rememberMeGroup">
+            {{-- <div class="form-group" id="rememberMeGroup">
                 <div class="controls">
                     <input type="checkbox" name="remember" id="remember_me" value="1"><label for="remember" class="remember-me-text">{{ __('voyager::generic.remember_me') }}</label>
                 </div>
@@ -69,5 +163,5 @@
                 </x-button>
             </div>
         </form>
-        @endsection        
+        @endsection         --}}
 
