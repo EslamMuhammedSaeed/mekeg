8c4ce070f6aa7996748f0160b6e1c3141881d261
Eslam <eslam.muhammed.saeed@gmail.com>
2021-05-15 04:04:46 +0200

    Procfile for Heroku6

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,28 +14,47 @@
             @csrf
 
             <!-- Email Address -->
-            <div class="form-group form-group-default" >
+            {{-- <div class="form-group form-group-default" >
                 <x-label for="email" :value="__('Email')" />
 
                 <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            </div> --}}
+
+            <div class="form-group form-group-default" id="emailGroup">
+                <label>{{ __('voyager::generic.email') }}</label>
+                <div class="controls">
+                    <input type="text" name="email" id="email" value="{{ old('email') }}" placeholder="{{ __('voyager::generic.email') }}" class="form-control" required>
+                </div>
             </div>
 
             <!-- Password -->
-            <div class="form-group form-group-default">
+            {{-- <div class="form-group form-group-default">
                 <x-label for="password" :value="__('Password')" />
 
                 <x-input id="password" class="block mt-1 w-full"
                                 type="password"
                                 name="password"
                                 required autocomplete="current-password" />
+            </div> --}}
+            <div class="form-group form-group-default" id="passwordGroup">
+                <label>{{ __('voyager::generic.password') }}</label>
+                <div class="controls">
+                    <input type="password" name="password" placeholder="{{ __('voyager::generic.password') }}" class="form-control" required>
+                </div>
             </div>
 
             <!-- Remember Me -->
-            <div class="block mt-4">
+            {{-- <div class="block mt-4">
                 <label for="remember_me" class="inline-flex items-center">
                     <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
                     <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                 </label>
+            </div> --}}
+
+            <div class="form-group" id="rememberMeGroup">
+                <div class="controls">
+                    <input type="checkbox" name="remember" id="remember_me" value="1"><label for="remember" class="remember-me-text">{{ __('voyager::generic.remember_me') }}</label>
+                </div>
             </div>
 
             <div class="flex items-center justify-end mt-4">
