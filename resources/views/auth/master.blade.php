042d118e155219cf9c9cedc8fc83e20add713b9d
Eslam <eslam.muhammed.saeed@gmail.com>
2021-05-18 23:01:31 +0200

    15 commit

--- resources/views/auth/master.blade.php
+++ resources/views/auth/master.blade.php
@@ -7,7 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
     <meta name="description" content="admin login">
     <title>MEKEG</title>
-    <link rel="stylesheet" href="{{ voyager_asset('css/app.css') }}">
+    {{-- <link rel="stylesheet" href="{{ voyager_asset('css/app.css') }}"> --}}
+    <link rel="stylesheet" href="{{asset('css/voyager/app.css') }}">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
